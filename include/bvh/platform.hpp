64668df7cb2244ae8ab6b1ddba378e2c96143322
Nicholas Yue <yue.nicholas@gmail.com>
2023-12-09 17:06:41 -0800

    chore: Apply clang-format to all header files

--- include/bvh/platform.hpp
+++ include/bvh/platform.hpp
@@ -1,18 +1,18 @@
 #ifndef BVH_PLATFORM_HPP
 #define BVH_PLATFORM_HPP
 
-#include <cstddef>
 #include <cassert>
+#include <cstddef>
 
 #ifdef _OPENMP
 #include <omp.h>
 #endif
 
 #if defined(__GNUC__) || defined(__clang__)
-#define bvh_restrict      __restrict
+#define bvh_restrict __restrict
 #define bvh_always_inline __attribute__((always_inline))
 #elif defined(_MSC_VER)
-#define bvh_restrict      __restrict
+#define bvh_restrict __restrict
 #define bvh_always_inline __forceinline
 #else
 #define bvh_restrict
@@ -20,23 +20,42 @@
 #endif
 
 #if defined(__GNUC__) || defined(__clang__)
-#define bvh_likely(x)   __builtin_expect(x, true)
+#define bvh_likely(x) __builtin_expect(x, true)
 #define bvh_unlikely(x) __builtin_expect(x, false)
 #else
-#define bvh_likely(x)   x
+#define bvh_likely(x) x
 #define bvh_unlikely(x) x
 #endif
 
-namespace bvh {
+namespace bvh
+{
 
 #ifdef _OPENMP
-inline size_t get_thread_count() { return omp_get_num_threads(); }
-inline size_t get_thread_id()    { return omp_get_thread_num(); }
-inline void assert_not_in_parallel() { assert(omp_get_level() == 0); }
-inline void assert_in_parallel() { assert(omp_get_level() > 0); }
+inline size_t get_thread_count()
+{
+  return omp_get_num_threads();
+}
+inline size_t get_thread_id()
+{
+  return omp_get_thread_num();
+}
+inline void assert_not_in_parallel()
+{
+  assert(omp_get_level() == 0);
+}
+inline void assert_in_parallel()
+{
+  assert(omp_get_level() > 0);
+}
 #else
-inline constexpr size_t get_thread_count() { return 1; }
-inline constexpr size_t get_thread_id()    { return 0; }
+inline constexpr size_t get_thread_count()
+{
+  return 1;
+}
+inline constexpr size_t get_thread_id()
+{
+  return 0;
+}
 inline void assert_not_in_parallel() {}
 inline void assert_in_parallel() {}
 #endif
--- include/bvh/sphere.hpp
+++ include/bvh/sphere.hpp
@@ -3,61 +3,67 @@
 
 #include <optional>
 
-#include "bvh/vector.hpp"
 #include "bvh/bounding_box.hpp"
 #include "bvh/ray.hpp"
+#include "bvh/vector.hpp"
 
-namespace bvh {
+namespace bvh
+{
 
 /// Sphere primitive defined by a center and a radius.
-template <typename Scalar>
-struct Sphere  {
-    struct Intersection {
-        Scalar  t;
+template<typename Scalar>
+struct Sphere
+{
+  struct Intersection
+  {
+    Scalar t;
 
-        Scalar distance() const { return t; }
-    };
+    Scalar distance() const { return t; }
+  };
 
-    using ScalarType       = Scalar;
-    using IntersectionType = Intersection;
+  using ScalarType = Scalar;
+  using IntersectionType = Intersection;
 
-    Vector3<Scalar> origin;
-    Scalar radius;
+  Vector3<Scalar> origin;
+  Scalar radius;
 
-    Sphere() = default;
-    Sphere(const Vector3<Scalar>& origin, Scalar radius)
-        : origin(origin), radius(radius)
-    {}
+  Sphere() = default;
+  Sphere(const Vector3<Scalar>& origin, Scalar radius)
+    : origin(origin)
+    , radius(radius)
+  {
+  }
 
-    Vector3<Scalar> center() const {
-        return origin;
+  Vector3<Scalar> center() const { return origin; }
+
+  BoundingBox<Scalar> bounding_box() const
+  {
+    return BoundingBox<Scalar>(origin - Vector3<Scalar>(radius), origin + Vector3<Scalar>(radius));
+  }
+
+  template<bool AssumeNormalized = false>
+  std::optional<Intersection> intersect(const Ray<Scalar>& ray) const
+  {
+    auto oc = ray.origin - origin;
+    auto a = AssumeNormalized ? Scalar(1) : dot(ray.direction, ray.direction);
+    auto b = 2 * dot(ray.direction, oc);
+    auto c = dot(oc, oc) - radius * radius;
+
+    auto delta = b * b - 4 * a * c;
+    if (delta >= 0)
+    {
+      auto inv = -Scalar(0.5) / a;
+      auto sqrt_delta = std::sqrt(delta);
+      auto t0 = (b + sqrt_delta) * inv;
+      if (t0 >= ray.tmin && t0 <= ray.tmax)
+        return std::make_optional(Intersection{ t0 });
+      auto t1 = (b - sqrt_delta) * inv;
+      if (t1 >= ray.tmin && t1 <= ray.tmax)
+        return std::make_optional(Intersection{ t1 });
     }
 
-    BoundingBox<Scalar> bounding_box() const {
-        return BoundingBox<Scalar>(origin - Vector3<Scalar>(radius), origin + Vector3<Scalar>(radius));
-    }
-
-    template <bool AssumeNormalized = false>
-    std::optional<Intersection> intersect(const Ray<Scalar>& ray) const {
-        auto oc = ray.origin - origin;
-        auto a = AssumeNormalized ? Scalar(1) : dot(ray.direction, ray.direction);
-        auto b = 2 * dot(ray.direction, oc);
-        auto c = dot(oc, oc) - radius * radius;
-
-        auto delta = b * b - 4 * a * c;
-        if (delta >= 0) {
-            auto inv = -Scalar(0.5) / a;
-            auto sqrt_delta = std::sqrt(delta);
-            auto t0 = (b + sqrt_delta) * inv;
-            if (t0 >= ray.tmin && t0 <= ray.tmax)
-                return std::make_optional(Intersection { t0 });
-            auto t1 = (b - sqrt_delta) * inv;
-            if (t1 >= ray.tmin && t1 <= ray.tmax)
-                return std::make_optional(Intersection { t1 });
-        }
-
-        return std::nullopt;
-    }
+    return std::nullopt;
+  }
 };
 
 } // namespace bvh
